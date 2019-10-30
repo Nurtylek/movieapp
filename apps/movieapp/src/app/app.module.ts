f9a9157c1db304c43153948a025492c46d38bc6e
Нуртилек Кошкар <nurtylek98@gmail.com>
2019-10-30 17:08:41 +0600

    Initial commit

--- apps/movieapp/src/app/app.module.ts
+++ apps/movieapp/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BackendModule } from '@movieapp/backend';
+import { SharedModule } from '@movieapp/shared'
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BackendModule,
+    SharedModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
