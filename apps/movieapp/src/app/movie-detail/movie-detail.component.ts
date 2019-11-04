e5fafae447a98c54068d21597d99a61547b87ba4
Нуртилек Кошкар <nurtylek98@gmail.com>
2019-11-04 11:39:59 +0600

    add type to paramMap

--- apps/movieapp/src/app/movie-detail/movie-detail.component.ts
+++ apps/movieapp/src/app/movie-detail/movie-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, Injector, OnInit } from '@angular/core';
 import { Abstract } from '../helpers/abstract';
 import { Movie } from '@movieapp/core';
 import { filter, map } from 'rxjs/operators';
+import { ParamMap } from '@angular/router';
 
 @Component({
     selector: 'movieapp-movie-detail',
@@ -15,12 +16,12 @@ export class MovieDetailComponent extends Abstract implements OnInit {
 
     ngOnInit() {
         this.route.paramMap.pipe(
-            filter(paramMap => paramMap.has('id')),
-            map(paramMap => +paramMap.get('id'))
+            filter((paramMap: ParamMap) => paramMap.has('id')),
+            map((paramMap: ParamMap) => +paramMap.get('id'))
         ).subscribe(id => this.getMovie(id));
     }
 
-    getMovie(id) {
+    getMovie(id: number) {
         this.backend.moviesService.getMovieById(id).subscribe((res: Movie) => {
             this.movie = res;
             this.getTheSameGenreMovies();
